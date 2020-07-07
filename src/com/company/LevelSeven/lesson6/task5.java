77d8c18d289bc4773a4484ad79f4fadbf2df3fe4
Maks-Somov <fail.epic2015@yandex.ru>
2020-07-07 20:14:36 +0300

    Level 7 lesson 6

--- src/com/company/LevelSeven/lesson6/task5.java
+++ src/com/company/LevelSeven/lesson6/task5.java
@@ -11,6 +11,7 @@ public class task5 {
     2. Добавь в него 5 строчек с клавиатуры.
     3. Удали последнюю строку и вставь её в начало. Повторить 13 раз.
     4. Используя цикл, выведи содержимое на экран, каждое значение с новой строки.*/
+
     public static void main(String[] args) throws IOException {
         List<String> list = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
