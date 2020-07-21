0e8b9a426cb7adf882799d7b39fcfaa28598f250
Maks-Somov <fail.epic2015@yandex.ru>
2020-07-21 22:16:35 +0300

    Level 15 lesson 4

---
+++ src/com/company/level15/lesson4/task3/Solution.java
@@ -0,0 +1,28 @@
+package com.company.level15.lesson4.task3;
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+/*
+    public static void print(byte b) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(double d) {
+        System.out.println("Я буду Java прогером!");
+    }
+*/
+    public static void print(long l) {
+        System.out.println("Я буду Java прогером!");
+    }
+/*
+    public static void print(float f) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(char c) {
+        System.out.println("Я буду Java прогером!");
+    }
+*/
+}
