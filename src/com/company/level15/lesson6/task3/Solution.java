d4f7595fed32774b2b19930a24c666b2bbe1d859
Maks-Somov <fail.epic2015@yandex.ru>
2020-07-21 22:39:59 +0300

    Level 15 lesson 6

---
+++ src/com/company/level15/lesson6/task3/Solution.java
@@ -0,0 +1,18 @@
+package com.company.level15.lesson6.task3;
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine implements Runnable {
+
+    }
+
+    public class Car extends Machine {
+
+    }
+}
